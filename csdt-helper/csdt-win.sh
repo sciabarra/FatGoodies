fb50f83ac11c4467d0a2716d9eeb1fc240cd47e8
Michele Sciabarra <michele@sciabarra.com>
2012-11-10 16:02:21 +0100

    readme for csdt-helper

--- csdt-helper/csdt-win.sh
+++ csdt-helper/csdt-win.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cp=$(ls lib/csdt-client-*)
 for i in lib/*.jar ; do cp="$cp;$i" ; done
 java -cp "$cp" com.fatwire.csdt.client.main.CSDT "$@" 
--- csdt-helper/local.sh
+++ csdt-helper/local.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-# configure here
-url=http://localhost:8080/cs/ContentServer
+set -x
+# configure here:
+url=http://localhost:8380/cs/ContentServer
 user=fwadmin
 pass=xceladmin
 site=avisports
-# uncomment here
+# uncomment here:
 sys=unix
 #sys=win
-#leave as is here
+#leave as it is here:
 cmd=${1:-listcs}
 res=${2:-@ALL_ASSETS}
-sh csdt-$sys.sh $url cmd="$cmd" username=$usr password=$pass fromSites=$site resources="$res"
+./csdt-$sys.sh $url cmd="$cmd" username=$user password=$pass fromSites=$site resources="$res"
